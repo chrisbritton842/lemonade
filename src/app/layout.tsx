522f7e731e392f6d19412738a90c92a2691b9929
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-11-18 15:38:46 -0800

    start header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Header } from "@/components/header";
 import { ThemeProvider } from "./provider";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
+          <Header />
           {children}
         </ThemeProvider>
       </body>
---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { landingPagePath, signInPagePath, signUpPagePath } from "@/paths";
+
+const Header = () => {
+    return (
+        <nav className="bg-white border-b border-gray-200 px-4 py-2 flex justify-between items-center">
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    );
+};
+
+export { Header };

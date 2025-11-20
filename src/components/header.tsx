f01ffb54fe3490292a58dd56fa11818de51e8c58
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-11-20 14:25:12 -0800

    add header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,46 @@
-import Link from "next/link";
+"use client";
+
+import { Box, Button, Flex, HStack, Link, Spacer,Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { landingPagePath, signInPagePath, signUpPagePath } from "@/paths";
 
 const Header = () => {
-    return (
-        <nav className="bg-white border-b border-gray-200 px-4 py-2 flex justify-between items-center">
-            <div>
+  return (
+    <Box
+      bg="white"
+      px={6}
+      py={4}
+      boxShadow="sm"
+      borderBottom="1px solid"
+      borderColor="gray.100"
+    >
+      <Flex align="center">
+        {/* Brand */}
+        <Link
+          as={NextLink}
+          href={landingPagePath()}
+          _hover={{ textDecoration: "none", color: "teal.600" }}
+        >
+          <Text fontSize="xl" fontWeight="bold" color="teal.500">
+            Lemonade
+          </Text>
+        </Link>
 
-            </div>
-            <div>
+        <Spacer />
 
-            </div>
-            <div>
+        {/* Navigation Buttons */}
+        <HStack gap={4}>
+          <Link as={NextLink} href={signUpPagePath()} _hover={{ textDecoration: "none" }}>
+            <Button colorScheme="teal">Sign Up</Button>
+          </Link>
 
-            </div>
-        </nav>
-    );
+          <Link as={NextLink} href={signInPagePath()} _hover={{ textDecoration: "none" }}>
+            <Button variant="outline" colorScheme="gray">Sign In</Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Box>
+  );
 };
 
 export { Header };

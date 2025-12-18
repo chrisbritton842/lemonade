5a4374040851c8db82ce89730dd90cde315dcfca
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-12-18 14:22:43 -0800

    add createSession function to src/lib/auth/session.ts

---
+++ src/lib/auth/session.ts
@@ -0,0 +1,23 @@
+import { randomBytes } from "crypto";
+import { prisma } from "@/lib/prisma";
+
+const SESSION_ACTIVE_DURATION = 1000 * 60 * 60 * 24 * 30; // 30 days
+const SESSION_IDLE_DURATION = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+export async function createSession(userId: string): Promise<{ sessionId: string;}> {
+  const sessionId = randomBytes(32).toString("hex");
+  const now = Date.now();;
+  const active_expires = BigInt(now + SESSION_ACTIVE_DURATION);
+  const idle_expires = BigInt(now + SESSION_IDLE_DURATION);
+
+  await prisma.session.create({
+    data: {
+      id: sessionId,
+      userId,
+      active_expires,
+      idle_expires,
+    },
+  });
+
+  return { sessionId };
+}

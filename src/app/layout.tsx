92b21aea07a50ae28ec7443ec5369b2c85fc2661
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-12-23 14:51:01 -0800

    add form component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./provider";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Header />
           {children}
+          <Toaster expand />
         </ThemeProvider>
       </body>
     </html>

c5171fe47565e5be07edbdaa95253282b1c599ef
Chris Britton <chrisbritton@Chriss-Laptop.local>
2026-05-26 13:43:14 -0700

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,54 @@
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { businessesPagePath,newBusinessPagePath } from "@/paths";
+
 const LandingPage = () => {
-  return <h2 className="text-lg">HomePage</h2>;
+  return (
+    <Center minHeight="calc(100vh - 80px)" bg="yellow.50" px={4}>
+      <Container maxW="3xl">
+        <Stack gap={8} textAlign="center" align="center">
+          <Box>
+            <Heading size="3xl" color="yellow.700">
+              Welcome to Lemonade, the all-in-one business management platform for kids!
+            </Heading>
+            <Text mt={4} fontSize="xl" color="gray.700">
+              Start a new business or join an existing business!
+            </Text>
+          </Box>
+          <HStack gap={4}>
+            <Button
+              asChild
+              size="lg"
+              colorPalette="yellow"
+            >
+              <NextLink href={newBusinessPagePath()}>
+                Start a New Business
+              </NextLink>
+            </Button>
+            <Button
+              asChild
+              size="lg"
+              variant="outline"
+              colorPalette="yellow"
+            >
+              <NextLink href={businessesPagePath()}>
+                Join an Existing Business
+              </NextLink>
+            </Button>
+          </HStack>
+        </Stack>
+      </Container>
+    </Center>
+  );
 };
 
 export default LandingPage;

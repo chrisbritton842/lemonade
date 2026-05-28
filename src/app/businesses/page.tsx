494e45fea3c0f57a25c693a8a2d935774576d980
Chris Britton <chrisbritton@Chriss-Laptop.local>
2026-05-28 11:11:12 -0700

    protect businesses page

--- src/app/businesses/page.tsx
+++ src/app/businesses/page.tsx
@@ -1,4 +1,14 @@
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth/get-current-user";
+import { signInPagePath } from "@/paths";
+
 const BusinessesPage = () => {
+    const currentUser = getCurrentUser();
+
+    if (!currentUser) {
+        redirect(signInPagePath());
+    }
+
     return <h2 className="text-lg">BusinessesPage</h2>;
 };
 
